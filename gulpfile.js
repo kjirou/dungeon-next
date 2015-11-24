5165b1fa5d2684cfbf3764e8989f23e9911c6a5b
kjirou <kjirou.web@gmail.com>
2015-11-25 01:54:32 +0900

    Bump up babel/babelify

--- gulpfile.js
+++ gulpfile.js
@@ -79,6 +79,10 @@ function createTransformer() {
   return babelify.configure({
     // Configure babel options here
     // Ref) http://babeljs.io/docs/usage/options/
+    presets: [
+      'es2015',
+      'react',
+    ]
   });
 }
 
